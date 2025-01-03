a54457a54182aab568eb3ae50728a525f783712d
Md jewel <jewel3472311@gmail.com>
2025-01-03 23:51:37 +0600

    added the blog components

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,22 +1,20 @@
 import { useEffect } from "react";
 import { useState } from "react";
 
-
 const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
 
-const [blogs, setBlogs] = useState([])
+  useEffect(() => {
+    fetch("fake.json")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
 
-useEffect(()=>{
-    fetch('fake.json')
-    .then(res => res.json())
-    .then(data => setBlogs(data))
-} ,[])
-
-    return (
-        <div>
-            <h2>blogs: {blogs.length}</h2>
-        </div>
-    );
+  return (
+    <header className="md:w-2/3">
+      <h2 className="text-4xl">Blogs</h2>
+    </header>
+  );
 };
 
 export default Blogs;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,12 @@
+import profile from "../../assets/images/profile.png";
 
 const Header = () => {
-    return (
-        <div>
-              <h1 className="text-6xl bg-red-400">Knowledge Cafe</h1>
-        </div>
-    );
+  return (
+    <div className="flex justify-between items-center p-4 mx-4 border-b-2">
+      <h1 className="text-4xl bg-red-400">Knowledge Cafe</h1>
+      <img src={profile} alt="" />
+    </div>
+  );
 };
 
 export default Header;
